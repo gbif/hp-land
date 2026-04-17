4f21a3a695bb177574cc810ba450a7da6a15d768
Jörg Holetschek <j.holetschek@bgbm.org>
2026-04-17 12:09:06 +0200

    siteID for Matomo corrected

--- _includes/js/measure.js
+++ _includes/js/measure.js
@@ -7,7 +7,7 @@ function attachMeasurements() {
   (function() {
     var u="https://stats.bo.berlin/";
     _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '1']);
+    _paq.push(['setSiteId', '39']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
   })();
